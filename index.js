a2b4a9519b5acad773c666a8194b4663b50c1b1e
Jerome Covington <jeromecovington@gmail.com>
2018-05-15 16:51:56 -0400

    Add doc block

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Compress query by stripping duplicate whitespace, and whitespace adjacent to
+ * brackets, braces, parentheses, colons, and commas.
+ *
+ * @param {string} text - the text to compress
+ * @returns {string} query - the compressed query
+ */
 module.exports = function graphQLCompress(text) {
   const query = text.replace(/\s+/g, ' ');
   return query.replace(/\s*(\[|\]|\{|\}|\(|\)|:|\,)\s*/g, '$1');
