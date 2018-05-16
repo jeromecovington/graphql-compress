6ac79bf8ec3a6b7948ee4dd37b4816a455e20f9b
Jerome Covington <jeromecovington@gmail.com>
2018-05-16 10:44:41 -0400

    Expand test for query w/o commas

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const graphQLCompress = require('../');
 
-const query = `
+const query1 = `
 {
   allSpecies (after: "c3BlY2llczox") {
     edges {
@@ -56,6 +56,64 @@ const query = `
 }
 `;
 
-const expected = '{allSpecies(after:"c3BlY2llczox"){edges{node{name,classification,designation,averageHeight,averageLifespan,eyeColors,hairColors,skinColors,language,homeworld{name,diameter,rotationPeriod,orbitalPeriod,gravity,population,climates,terrains,surfaceWater,residentConnection{edges{node{name,birthYear,eyeColor,gender,hairColor,height,mass,skinColor,created,edited,id}}},created,edited,id},created,edited,id}}}}';
+const expected1 = '{allSpecies(after:"c3BlY2llczox"){edges{node{name,classification,designation,averageHeight,averageLifespan,eyeColors,hairColors,skinColors,language,homeworld{name,diameter,rotationPeriod,orbitalPeriod,gravity,population,climates,terrains,surfaceWater,residentConnection{edges{node{name,birthYear,eyeColor,gender,hairColor,height,mass,skinColor,created,edited,id}}},created,edited,id},created,edited,id}}}}';
 
-assert.strictEqual(graphQLCompress(query), expected);
+assert.strictEqual(graphQLCompress(query1), expected1);
+
+const query2 = `
+{
+  allSpecies (after: "c3BlY2llczox") {
+    edges {
+      node {
+        name
+        classification
+        designation
+        averageHeight
+        averageLifespan
+        eyeColors
+        hairColors
+        skinColors
+        language
+        homeworld {
+          name
+          diameter
+          rotationPeriod
+          orbitalPeriod
+          gravity
+          population
+          climates
+          terrains
+          surfaceWater
+          residentConnection {
+            edges {
+              node {
+                name
+                birthYear
+                eyeColor
+                gender
+                hairColor
+                height
+                mass
+                skinColor
+                created
+                edited
+                id
+              }
+            }
+          }
+          created
+          edited
+          id
+        }
+        created
+        edited
+        id
+      }
+    }
+  }
+}
+`;
+
+const expected2 = '{allSpecies(after:"c3BlY2llczox"){edges{node{name classification designation averageHeight averageLifespan eyeColors hairColors skinColors language homeworld{name diameter rotationPeriod orbitalPeriod gravity population climates terrains surfaceWater residentConnection{edges{node{name birthYear eyeColor gender hairColor height mass skinColor created edited id}}}created edited id}created edited id}}}}'
+
+assert.strictEqual(graphQLCompress(query2), expected2);
